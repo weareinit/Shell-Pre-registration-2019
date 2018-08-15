b497d33d65fe569940ea14469c14ce060307e93d
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2018-08-15 16:53:46 -0500

    That big O

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,17 @@ let sponsors = JSON.parse(sponsorJson);
 let addSponsors = () => {
   sponsors.forEach(sponsor => {
   
-  let image = $('<img>');
-  image.attr('src', 'imgs/logos/2018/' + sponsor.logo);
-  image.attr('title', sponsor.name);
-    
-  let link = $('<a>');
-  link.attr('href', sponsor.url);
-  link.attr('target', '_blank');
-  link.addClass(sponsor.tier + ' ' + sponsor.name.toLocaleLowerCase().replace(/ /gi, '-'));
-  
-  link.append(image);
-  
-  $('#' + sponsor.tier +'-sponsors').append(link);
-});
+      let image = $('<img>');
+      image.attr('src', 'imgs/logos/2018/' + sponsor.logo);
+      image.attr('title', sponsor.name);
+
+      let link = $('<a>');
+      link.attr('href', sponsor.url);
+      link.attr('target', '_blank');
+      link.addClass(sponsor.tier + ' ' + sponsor.name.toLocaleLowerCase().replace(/ /gi, '-'));
+
+      link.append(image);
+
+      $('#' + sponsor.tier +'-sponsors').append(link);
+  });
 }
