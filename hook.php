869a0301dceeb6404ef9298b46fa00e6cbda8ec2
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2018-08-15 13:33:01 -0500

    Testing hook

--- hook.php
+++ hook.php
@@ -1,5 +1,7 @@
 <?php
 include 'config.php';
+
+//test
 // what command to execute upon retrieval of a valid push event
 $cmd = 'git pull';
 // the shared secret, used to sign the POST data (using HMAC with SHA1)
